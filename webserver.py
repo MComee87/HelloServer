36428e25c624652c82b2ae0d8683990bd038121d
Matthew Comee <MComee87@users.noreply.github.com>
2017-03-10 05:19:19 +0000

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 class webserverHandler(BaseHTTPRequestHandler):
         def do_GET(self):
@@ -9,7 +10,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                                 self.end_headers()
 
                                 output = ""
-                                output += "<html><body><p style='color:blue; background-color:purple; margin-right:auto; margin-left:auto; font-size:72px; font-family: 'cursive;'>Hello!</p> <a href = '/index'>Back to home</a></body></html>"
+                                output += "<html><body><p style='color:blue; background-color:purple; margin-right:auto; margin-left:auto; font-size:72px; font-family: 'cursive;'>Hello!</p> <a href = '/index'>Back to home</a>"
+                                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'> </form>"
+                                output += "</body></html>"
                                 self.wfile.write(output)
                                 print output
                                 return
@@ -20,7 +23,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                                 self.end_headers()
 
                                 output = ""
-                                output += "<html><body><p style='color:purple; background-color:blue; margin-right:auto; margin-left:auto; font-size:72px; font-family: 'cursive;'>&#161Hola!</p> <a href = '/index'>Back to home</a></body></html>"
+                                output += "<html><body><p style='color:purple; background-color:blue; margin-right:auto; margin-left:auto; font-size:72px; font-family: 'cursive;'>&#161Hola!</p> <a href = '/index'>Back to home</a>"
+                                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'> </form>"
+                                output += "</body></html>"
                                 self.wfile.write(output)
                                 print output
                                 return
@@ -40,6 +45,30 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                 except IOError:
                         self.send_error(404, "File Not Found %s" % self.path)
+
+
+        def do_POST(self):
+                try:
+                        self.send_response(301)
+                        self.end_headers()
+
+                        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                        if ctype == 'multipart/form-data':
+                                fields=cgi.parse_multipart(self.rfile, pdict)
+                                messagecontent = fields.get('message')
+
+                        output = ""
+                        output += "<html><body>"
+                        output += "<h2>Okay, how about this:</h2>"
+                        output += "<h1> %s </h1>" % messagecontent[0]
+
+                        output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'> </form>"
+                        output += "</body></html>"
+                        self.wfile.write(output)
+                        print output
+                        
+                except:
+                        pass
 
 
 def main():
